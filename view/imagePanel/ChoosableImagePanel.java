7fe177c5a4a088a4a1ebd97f8040fbc261d9a4e4
Tomasz Trębski <kornicameister@gmail.com>
2012-04-15 01:39:40 +0200

    added new class that extends existing ImagePanel with ability to be marked

---
+++ view/imagePanel/ChoosableImagePanel.java
@@ -0,0 +1,120 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.imagePanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.logging.Level;
+import javax.swing.ImageIcon;
+import logger.MabisLogger;
+
+/**
+ *
+ * @author kornicameister
+ */
+public class ChoosableImagePanel extends ImagePanel{
+
+    private Boolean marked;
+    private final MouseListener listener;
+    private Color defaultColor;
+    private static final Integer borderShift = 10;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+    public ChoosableImagePanel(ImageIcon img, String path) {
+        super(img, path);
+        this.marked = false;
+        this.defaultColor = this.getForeground();
+        this.listener = new ChoosableImagePanelMouseListener();
+        this.addMouseListener(this.listener);
+    }
+
+    /**
+     * The status of imagePanel, if panel detected clicked within it's area than
+     * {@link ChoosableImagePanel#marked} value is true, otherwise is set to
+     * false
+     *
+     * @return the marked
+     */
+    public Boolean getMarked() {
+        return marked;
+    }
+
+    /**
+     * Calling this method causes PropertyChangeSupport to fire property change event
+     * that can be caught in adjusted class
+     * @param marked the marked to set
+     */
+    public void setMarked(Boolean marked) {
+        Boolean oldMarked = this.marked;
+        this.marked = marked;
+        this.pcs.firePropertyChange("panelMarked", oldMarked, this.marked);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l){
+        this.pcs.addPropertyChangeListener(l);
+    }
+    
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l){
+        this.pcs.removePropertyChangeListener(l);
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        int topLeftX = getWidth() / 10;
+        int topLeftY = getWidth() / 7;
+        rescaleImage();
+
+        if (this.marked) {
+            g.setColor(Color.gray.brighter().brighter());
+            g.fill3DRect(0, 0, scaledImage.getIconWidth(), scaledImage.getIconHeight(), false);
+        } else {
+            g.setColor(this.defaultColor);
+            g.clearRect(0, 0, scaledImage.getIconWidth(), scaledImage.getIconHeight());
+        }
+        g.drawImage(scaledImage.getImage(), topLeftX, topLeftY, scaledImage.getIconWidth() - padding,
+                scaledImage.getIconHeight() - padding, null);
+    }
+
+    /**
+     * Works pretty much the same as {@link ChoosableImagePanel#setMarked(false)} but without
+     * triggering property change event, additionaly it fires repainting of panel
+     */
+    public void demark() {
+        this.marked = false;
+        this.repaint();
+    }
+    
+    /**
+     * Works like {@link ChoosableImagePanel#setMarked(true)}, but does not trigger fire property change
+     */
+    public void mark() {
+        this.marked = true;
+        this.repaint();
+    }
+
+    class ChoosableImagePanelMouseListener extends MouseAdapter implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent o) {
+            Point p = o.getLocationOnScreen();
+            Point pp = getLocationOnScreen();
+            boolean inArea = p.getX() > pp.getX() & p.getX() < (pp.getX() + getWidth());
+            inArea = inArea & (p.getY() > pp.getY() & p.getY() < (pp.getY() + getHeight()));
+            if (inArea) {
+                setMarked(!marked);
+                MabisLogger.getLogger().log(Level.INFO, "ChoosableImagePanel click detected, status: {0}", (marked ? "marked" : "unmarked"));
+            }
+            repaint();
+        }
+    }
+}
--- view/imagePanel/ImagePanel.java
+++ view/imagePanel/ImagePanel.java
@@ -10,79 +10,77 @@ import javax.swing.JPanel;
  * Class is to conveniently place an image inside of JPanel By Extending JPanel
  * user is given with ability of using standard JPanel functionality such as for
  * example creating borders
- * 
+ *
  * @author kornicameister
  * @see JPanel
  * @version 0.2
  */
 public class ImagePanel extends JPanel {
-	private static final long serialVersionUID = 8841755218083931060L;
-	private static final Integer padding = 10;
-	private ImageIcon img = null, scaledImage = null;
-	private String imgPath;
+    private static final long serialVersionUID = 8841755218083931060L;
+    protected static final Integer padding = 10;
+    protected ImageIcon img = null, scaledImage = null;
+    private String imgPath;
 
-	public ImagePanel(ImageIcon img, String path) {
-		super(true);
-		this.img = img;
-		this.imgPath = (path == null ? "" : path);
-	}
+    public ImagePanel(ImageIcon img, String path) {
+        super(true);
+        this.img = img;
+        this.imgPath = (path == null ? "" : path);
+    }
 
-	@Override
-	public void setSize(Dimension d) {
-		super.setSize(d);
-	}
+    @Override
+    public void setSize(Dimension d) {
+        super.setSize(d);
+    }
 
-	@Override
-	public void setSize(int w, int h) {
-		super.setSize(w, h);
-	}
+    @Override
+    public void setSize(int w, int h) {
+        super.setSize(w, h);
+    }
 
-	public void setImg(ImageIcon img, String path) {
-		this.img = null;
-		this.img = img;
-		this.imgPath = (path == null ? "" : path);
-	}
+    public void setImg(ImageIcon img, String path) {
+        this.img = null;
+        this.img = img;
+        this.imgPath = (path == null ? "" : path);
+    }
 
-	public ImageIcon getImage() {
-		return img;
-	}
+    public ImageIcon getImage() {
+        return img;
+    }
 
-	public String getImagePath() {
-		return imgPath;
-	}
+    public String getImagePath() {
+        return imgPath;
+    }
 
-	/**
-	 * Method rescales internal image to adjust it to panel size <b>Notice</b>
-	 * that image size in certain dimension will be always set to smaller in
-	 * following comparision:
-	 * <ul>
-	 * <li>imageSize.width vs imagePanel.width -> smaller will be chosen</li>
-	 * <li>imageSize.height vs imagePanel.height -> smaller will be chosen</li>
-	 * </ul>
-	 * 
-	 * @param d
-	 * @see ImagePanel#imageSize
-	 */
-	private void rescaleImage() {
-		this.scaledImage = new ImageIcon(this.img.getImage().getScaledInstance(
-				this.getWidth() - padding, this.getHeight() - padding,
-				0));
-	}
+    /**
+     * Method rescales internal image to adjust it to panel size <b>Notice</b>
+     * that image size in certain dimension will be always set to smaller in
+     * following comparision: <ul> <li>imageSize.width vs imagePanel.width ->
+     * smaller will be chosen</li> <li>imageSize.height vs imagePanel.height ->
+     * smaller will be chosen</li> </ul>
+     *
+     * @param d
+     * @see ImagePanel#imageSize
+     */
+    protected void rescaleImage() {
+        this.scaledImage = new ImageIcon(this.img.getImage().getScaledInstance(
+                this.getWidth() - padding, this.getHeight() - padding,
+                0));
+    }
 
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		rescaleImage();
-		int topLeftX = getWidth() / 10;
-		int topLeftY = getWidth() / 7;
-		g.drawImage(scaledImage.getImage(), topLeftX, topLeftY, scaledImage.getIconWidth()-padding,
-				scaledImage.getIconHeight()-padding, null);
-	}
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        rescaleImage();
+        int topLeftX = getWidth() / 10;
+        int topLeftY = getWidth() / 7;
+        g.drawImage(scaledImage.getImage(), topLeftX, topLeftY, scaledImage.getIconWidth() - padding,
+                scaledImage.getIconHeight() - padding, null);
+    }
 
-	@Override
-	public String toString() {
-		return "ImagePanel [(" + this.getWidth() + "," + this.getHeight()
-				+ ")\n(" + this.imgPath + ")\n(" + this.img.getIconWidth()
-				+ "," + this.img.getIconHeight() + ")]";
-	}
+    @Override
+    public String toString() {
+        return "ImagePanel [(" + this.getWidth() + "," + this.getHeight()
+                + ")\n(" + this.imgPath + ")\n(" + this.scaledImage.getIconWidth()
+                + "," + this.scaledImage.getIconHeight() + ")]";
+    }
 }
