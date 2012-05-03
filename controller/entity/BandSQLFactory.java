046048b5e0f2f07c6a42e00b74d0961af94265a9
Tomasz Trębski <kornicameister@gmail.com>
2012-05-03 20:25:26 +0200

    wrong name for the class

--- controller/entity/BandSQLFactory.java
+++ controller/entity/BandSQLFactory.java
@@ -3,14 +3,17 @@ package controller.entity;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.TreeSet;
 
 import model.BaseTable;
+import model.entity.Band;
 import controller.SQLFactory;
 import controller.SQLStamentType;
 
-public class BrandSQLFactory extends SQLFactory {
+public class BandSQLFactory extends SQLFactory {
+	private final TreeSet<Band> bands = new TreeSet<Band>();
 
-	public BrandSQLFactory(SQLStamentType type, BaseTable table) {
+	public BandSQLFactory(SQLStamentType type, BaseTable table) {
 		super(type, table);
 		// TODO Auto-generated constructor stub
 	}
@@ -26,6 +29,15 @@ public class BrandSQLFactory extends SQLFactory {
 	protected void parseResultSet(ResultSet set) throws SQLException {
 		// TODO Auto-generated method stub
 
+	}
+
+	/**
+	 * Zwraca pobrane zespoły
+	 * 
+	 * @return the bands
+	 */
+	public TreeSet<Band> getBands() {
+		return bands;
 	}
 
 }
