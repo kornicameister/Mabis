2d202f8d99b9b723cd8f2b32cbea44b3372519b8
Tomasz Trębski <kornicameister@gmail.com>
2012-05-03 13:34:13 +0200

    usersqlfactory ready for testing:

--- controller/entity/UserSQLFactory.java
+++ controller/entity/UserSQLFactory.java
@@ -1,19 +1,12 @@
 package controller.entity;
 
-import java.io.FileNotFoundException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.logging.Level;
 
-import logger.MabisLogger;
-import model.entity.Picture;
 import model.entity.User;
-import model.enums.ImageType;
-import model.utilities.ForeignKey;
-import settings.GlobalPaths;
-import utilities.Hasher;
+import utilities.Utilities;
 import controller.SQLFactory;
 import controller.SQLStamentType;
 
@@ -39,31 +32,18 @@ public class UserSQLFactory extends SQLFactory {
 		case UPDATE:
 			break;
 		case INSERT:
-			short index = 1;
-			st.setInt(index++, this.insertAvatar());
-			st.setString(index++, u.getLastName());
-			st.setString(index++, u.getFirstName());
-			st.setString(index++, Hasher.hashPassword(u.getPassword()));
-			st.setString(index++, u.getEmail());
-			st.setString(index++, u.getLogin());
+			st.setObject(1, u);
+			st.setInt(2, this.insertAvatar());
 			st.execute();
-
-			// getting last inserted id
 			st.clearParameters();
-			String query = "SELECT id! from mabis.! order by id! desc limit 1";
-			String tableName = String.valueOf(
-					this.table.getTableName().charAt(0)).toUpperCase()
-					+ this.table.getTableName().substring(1);
-
-			query = query.replaceFirst("!", tableName);
-			query = query.replaceFirst("!", this.table.getTableName());
-			query = query.replaceFirst("!", tableName);
-			this.parseResultSet(st.executeQuery(query));
+			this.lastAffactedId = Utilities.lastInsertedId(u, st);
 			break;
 		case SELECT:
 			this.parseResultSet(st.executeQuery());
 			break;
 		case DELETE:
+			st.setInt(1, u.getPrimaryKey());
+			this.parseDeleteSet(st.executeUpdate());
 			break;
 		default:
 			break;
@@ -72,54 +52,22 @@ public class UserSQLFactory extends SQLFactory {
 
 	@Override
 	final protected void parseResultSet(ResultSet set) throws SQLException {
-		User u = null;
+		User user = null;
 		switch (this.type) {
 		case SELECT:
 			while (set.next()) {
-				u = new User();
-				u.setFirstName(set.getString("firstName"));
-				u.setLastName(set.getString("lastName"));
-				u.setEmail(set.getString("email"));
-				u.setLogin(set.getString("login"));
-				u.setPassword(set.getString("password"));
-				u.setPrimaryKey(set.getInt("idUser"));
-				u.setPicture(this.selectAvatar(set.getString("image"),
-						set.getString("hash")));
-				u.addForeingKey(new ForeignKey(u.getPictureFile(), "avatar",
-						set.getInt("idPicture")));
-				this.users.put(u.getPrimaryKey(), u);
+				Object oo = set.getObject("object");
+				if (oo instanceof User) {
+					user = (User) oo;
+					this.users.put(user.getPrimaryKey(), user);
+				}
 			}
 			break;
 		case UPDATE:
 			break;
-		case DELETE:
-			break;
-		case INSERT:
-			while (set.next()) {
-				this.lastAffactedId = set.getInt(1);
-			}
-			break;
 		default:
 			break;
 		}
-	}
-
-	private Picture selectAvatar(String path, String checkSum) {
-		Picture p = null;
-		try {
-			p = new Picture(path, ImageType.AVATAR);
-			if (p.getCheckSum().equals(checkSum)) {
-				return p;
-			} else {
-				MabisLogger.getLogger().log(Level.WARNING,
-						"Checksum of images does not match");
-				p = new Picture(GlobalPaths.DEFAULT_COVER_PATH.toString(),
-						ImageType.UNDEFINED);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return p;
 	}
 
 	private Integer insertAvatar() throws SQLException {
