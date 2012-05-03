660bfb31527290627d424936555911f5dcaebc53
Tomasz Trębski <kornicameister@gmail.com>
2012-05-03 21:29:43 +0200

    BandSQLFactory extends AuthorSQLFactory, because select and insert statements are identical for both wrappers as Band entity extends Author in context of the model

--- controller/entity/BandSQLFactory.java
+++ controller/entity/BandSQLFactory.java
@@ -1,34 +1,99 @@
 package controller.entity;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 import model.BaseTable;
 import model.entity.Band;
-import controller.SQLFactory;
+import model.entity.Genre;
 import controller.SQLStamentType;
 
-public class BandSQLFactory extends SQLFactory {
+public class BandSQLFactory extends AuthorSQLFactory {
 	private final TreeSet<Band> bands = new TreeSet<Band>();
 
 	public BandSQLFactory(SQLStamentType type, BaseTable table) {
 		super(type, table);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void executeByTableAndType(PreparedStatement st)
 			throws SQLException {
-		// TODO Auto-generated method stub
+		Band band = (Band) this.table;
+		switch (this.type) {
+		case INSERT:
+			this.insertGenres(band.getTagCloud());// inserting genres
+			this.insertEntity(band, st);// inserting band itself
+			break;
+		case DELETE:
+			st.setInt(1, band.getPrimaryKey());
+			this.parseDeleteSet(st.executeUpdate());
+			break;
+		case UPDATE:
+		case SELECT:
+			this.parseResultSet(st.executeQuery());
+			break;
+		default:
+			break;
+		}
+	}
 
+	/**
+	 * Metoda stara się umieścić w bazie danych nowe gatunki. Niemniej, jeśli
+	 * dany gatunek jest już w bazie danych, to nie jest on ponownie
+	 * umieszczany.
+	 * 
+	 * @param genres
+	 * @throws SQLException
+	 */
+	private void insertGenres(ArrayList<Genre> genres) throws SQLException {
+		GenreSQLFactory gsf = new GenreSQLFactory(SQLStamentType.SELECT,
+				genres.get(0));
+		gsf.reset();
+		for (Genre genre : genres) {
+			gsf.setGenre(genre);
+			gsf.addWhereClause("idGenre", genre.getPrimaryKey().toString());
+			gsf.executeSQL(true);
+			if (gsf.getGenres().isEmpty()) {
+				gsf.setStatementType(SQLStamentType.INSERT);
+				this.lastAffactedId = gsf.executeSQL(true);
+				genre.setPrimaryKey(genre.getPrimaryKey());
+			}
+			gsf.reset();
+		}
 	}
 
 	@Override
 	protected void parseResultSet(ResultSet set) throws SQLException {
-		// TODO Auto-generated method stub
-
+		Band band = null;
+		switch (this.type) {
+		case SELECT:
+			while (set.next()) {
+				byte[] buf = set.getBytes("object");
+				if (buf != null) {
+					try {
+						ObjectInputStream objectIn = new ObjectInputStream(
+								new ByteArrayInputStream(buf));
+						band = (Band) objectIn.readObject();
+						band.setPrimaryKey(set.getInt("idBand"));
+						this.bands.add(band);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+				band = null;
+			}
+			break;
+		default:
+			break;
+		}
 	}
 
 	/**
