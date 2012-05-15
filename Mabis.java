b94ad8b27f153cfa324c9264b2677bc2183eea4d
Tomasz Trębski <kornicameister@gmail.com>
2012-05-15 21:24:17 +0200

    added special operation to clear tmp folder

--- Mabis.java
+++ Mabis.java
@@ -1,13 +1,24 @@
+import java.io.File;
+
+import settings.GlobalPaths;
 import view.items.BookCreator;
 
 public class Mabis {
 	public static void main(String[] args) {
+
+		// deleting cache, right before application starts
+		File tmp = new File(GlobalPaths.TMP.toString());
+		File[] files = tmp.listFiles();
+		for (File f : files) {
+			f.delete();
+		}
+
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-//				MainWindow mw = new MainWindow("MABIS",
-//						new Dimension(1000, 1000));
-//				mw.setVisible(true);
+				// MainWindow mw = new MainWindow("MABIS",
+				// new Dimension(1000, 1000));
+				// mw.setVisible(true);
 				BookCreator mw = new BookCreator("New book");
 				mw.setVisible(true);
 			}
