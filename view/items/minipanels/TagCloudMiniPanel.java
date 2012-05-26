d251402184dd046d302fcfcedf068635e6951ebb
Tomasz Trębski <kornicameister@gmail.com>
2012-05-27 00:46:53 +0200

    new functionality of tag cloud is defined, up and running, will be used widespread

--- view/items/minipanels/TagCloudMiniPanel.java
+++ view/items/minipanels/TagCloudMiniPanel.java
@@ -1,52 +1,60 @@
 package view.items.minipanels;
 
-import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import model.entity.Genre;
-import controller.SQLStamentType;
-import controller.entity.GenreSQLFactory;
+import model.enums.GenreType;
 
 /**
  * Klasa symuluje chmurę tagów. Tagi mogą być do niej dodawana pojedynczo lub
- * jako cała {@link ArrayList}
+ * jako cała {@link ArrayList}. Wewnętrznie korzysta z {@link GenreMiniPanel},
+ * który pozwala na wybiór i dodawania pojedynczych gatunków.
  * 
  * @author kornicameister
+ * @see GenreMiniPanel
  * 
  */
 public class TagCloudMiniPanel extends JPanel {
 	private static final long serialVersionUID = -8170767178911147951L;
-	private ArrayList<Genre> tags = new ArrayList<>();
-	private TreeMap<Genre, JLabel> tagToLabel = new TreeMap<>();
-	private JPanel contentPanel = new JPanel();
+	private JTable tagsTable = new JTable();
+	private TreeMap<Genre, Integer> genreToRow = new TreeMap<Genre, Integer>();
+	private DefaultTableModel tagsModel;
 	private GenreMiniPanel gmp;
+	private JScrollPane scrollForTable;
 
-	public TagCloudMiniPanel() {
-		// this.contentPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		this.contentPanel.setLayout(new BoxLayout(this.contentPanel,
-				BoxLayout.Y_AXIS));
+	public TagCloudMiniPanel(TreeSet<Genre> genres, GenreType type) {
+		gmp = new GenreMiniPanel(genres, type);
+		this.addPropertyListener();
+		this.initializeTagTable();
+		this.layoutComponents();
+	}
 
-		GenreSQLFactory gsf = new GenreSQLFactory(SQLStamentType.SELECT,
-				new Genre());
-		try {
-			gsf.executeSQL(true);
-			gmp = new GenreMiniPanel("Tags", gsf.getGenres());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		this.add(gmp, BorderLayout.PAGE_START);
-		this.add(this.contentPanel);
+	private void initializeTagTable() {
+		String columnNames[] = { "LP", "ID", "Name" };
+		this.tagsModel = new DefaultTableModel(columnNames, 0);
+		this.tagsTable = new JTable(this.tagsModel);
+		this.tagsTable.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		this.tagsTable.setEnabled(false);
+	}
 
-		this.gmp.getGenreBox().setVisible(false);
+	private void addPropertyListener() {
 		this.gmp.addPropertyChangeListener(new PropertyChangeListener() {
 
 			@Override
@@ -54,61 +62,157 @@ public class TagCloudMiniPanel extends JPanel {
 				String property = e.getPropertyName();
 				if (property.equals("genreSelected")
 						|| property.equals("genreCreated")) {
-					Genre tmp = (Genre) e.getNewValue();
-					if (tags.size() == 0) {
-						addTag(tmp);
-						return;
-					}
-					for (Genre g : tags) {
-						if (!g.getGenre().equals(tmp.getGenre())) {
-							addTag(g);
-							return;
-						}
-					}
+					addTag((Genre) e.getNewValue());
 				}
 			}
 		});
 	}
 
-	void addTag(Genre g) {
-		if (!this.tags.contains(g)) {
-			this.tags.add(g);
-			JLabel tmp = new JLabel(g.toString());
-			this.tagToLabel.put(g, tmp);
-			this.contentPanel.add(tmp);
-		}
+	private void layoutComponents() {
+		this.scrollForTable = new JScrollPane(this.tagsTable);
+
+		GroupLayout gl = new GroupLayout(this);
+		this.setLayout(gl);
+
+		gl.setHorizontalGroup(gl.createSequentialGroup()
+				.addComponent(this.gmp, 100, 100, 100).addGap(5)
+				.addComponent(this.scrollForTable));
+
+		gl.setVerticalGroup(gl.createParallelGroup().addComponent(this.gmp)
+				.addComponent(this.scrollForTable, 80, 80, 80));
+
+		this.tagsTable.getColumnModel().getColumn(0).setMaxWidth(40);
+		this.tagsTable.getColumnModel().getColumn(1).setMaxWidth(40);
 	}
 
-	public
-	void setTags(Collection<Genre> genres) {
-		this.tags.clear();
-		for(Genre g : genres){
-			this.tags.add(g);
-		}
-		this.contentPanel.removeAll();
-		for (Genre g : this.tags) {
-			JLabel tmp = new JLabel(g.toString());
-			this.contentPanel.add(tmp);
-			this.tagToLabel.put(g, tmp);
-		}
+	public void addTag(Genre g) {
+		Object data[] = { this.genreToRow.size()+1,g.getPrimaryKey(), g.getGenre() };
+		this.tagsModel.addRow(data);
+		this.genreToRow.put(g, this.genreToRow.size());
 	}
 
-	void removeTag(Genre g) {
-		this.tags.remove(g);
-		this.contentPanel.remove(this.tagToLabel.get(g));
-		this.repaint();
+	public void setTags(Collection<Genre> genres) {
+		gmp.tags.clear();
+		for (Genre g : genres) {
+			gmp.tags.add(g);
+			Object data[] = { g.getPrimaryKey(), g.getGenre() };
+			this.tagsModel.addRow(data);
+			this.genreToRow.put(g, this.genreToRow.size());
+		}
+		this.tagsTable.revalidate();
+	}
+
+	public void removeTag(Genre g) {
+		gmp.tags.remove(g);
+		this.tagsModel.removeRow(this.genreToRow.get(g));
+		this.genreToRow.remove(g);
+		this.tagsTable.revalidate();
 	}
 
 	public void clear() {
-		this.tags.clear();
-		this.contentPanel.removeAll();
-		this.repaint();
+		gmp.tags.clear();
+		for (int i = tagsModel.getRowCount(); i > 0; i--) {
+			this.tagsModel.removeRow(i);
+		}
+		this.tagsTable.revalidate();
+		this.genreToRow.clear();
+	}
+
+	public ArrayList<Genre> getTags() {
+		return gmp.tags;
 	}
 
 	/**
-	 * @return the tags
+	 * Wewnętrzna klasa, definiuje funkcjonalność dodawania nowego gatunku bądź
+	 * wybieranie gatunków z tych już istniejących. Nie pozwala na dodanie
+	 * gatunku takiego, który co do nazwy jest identyczny z gatunkiem już
+	 * istniejącym.
+	 * 
+	 * @author kornicameister
+	 * 
 	 */
-	public ArrayList<Genre> getTags() {
-		return tags;
+	private class GenreMiniPanel extends JPanel implements ActionListener {
+		private static final long serialVersionUID = -9003039695215128758L;
+		private JButton newGenreButton = new JButton("N");
+		private JButton selectGenreButton = new JButton("S");
+		private GenreType type;
+
+		final ArrayList<Genre> tags = new ArrayList<>();
+
+		public GenreMiniPanel(TreeSet<Genre> treeSet, GenreType type) {
+			this.tags.addAll(treeSet);
+			this.initComponents();
+			this.layoutComponents();
+			this.type = type;
+		}
+
+		private void layoutComponents() {
+			JPanel tmp = new JPanel();
+			GroupLayout gl = new GroupLayout(tmp);
+			tmp.setLayout(gl);
+
+			gl.setHorizontalGroup(gl.createParallelGroup()
+					.addComponent(this.newGenreButton, 50, 50, 50)
+					.addComponent(this.selectGenreButton, 50, 50, 50));
+			gl.setVerticalGroup(gl.createSequentialGroup()
+					.addComponent(this.newGenreButton)
+					.addComponent(this.selectGenreButton));
+			this.add(tmp);
+		}
+
+		private void initComponents() {
+			this.newGenreButton.setToolTipText("Create new genre");
+			this.newGenreButton.setName("Create new genre");
+			this.newGenreButton.addActionListener(this);
+			this.selectGenreButton.setToolTipText("Select genre");
+			this.selectGenreButton.setName("Select genre");
+			this.selectGenreButton.addActionListener(this);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton source = (JButton) e.getSource();
+			if (source.equals(newGenreButton)) {
+				String returned = JOptionPane.showInputDialog(null, "Input:",
+						source.getName(), JOptionPane.PLAIN_MESSAGE);
+				if (returned != null) {
+					Genre tmp = new Genre(returned, this.type);
+					int found = Collections.binarySearch(this.tags, tmp,
+							new Comparator<Genre>() {
+								@Override
+								public int compare(Genre o1, Genre o2) {
+									return o1.getGenre().compareTo(
+											o2.getGenre());
+								}
+							});
+					if (found < 0) {
+						this.tags.add(tmp);
+					}
+					this.firePropertyChange("genreCreated", null, tmp);
+				}
+			} else if (source.equals(selectGenreButton)) {
+				if (tags.size() == 0) {
+					return;
+				}
+				Object arr[] = tags.toArray();
+				Object returned = JOptionPane.showInputDialog(source,
+						"Select one from following box", source.getName(),
+						JOptionPane.QUESTION_MESSAGE, null, arr, arr[0]);
+				if (returned != null) {
+					Genre tmp = (Genre) returned;
+					int found = Collections.binarySearch(this.tags, tmp,
+							new Comparator<Genre>() {
+								@Override
+								public int compare(Genre o1, Genre o2) {
+									return o1.getGenre().compareTo(
+											o2.getGenre());
+								}
+							});
+					if (found < 0) {
+						this.firePropertyChange("genreSelected", null, tmp);
+					}
+				}
+			}
+		}
 	}
 }
