27639c7e04daf4a0f27cbffaee30688bfd179097
kornicameister <kornicameister@gmail.com>
2012-06-02 15:38:20 +0200

    removing files

--- settings/SettingsParseException.java
+++
@@ -1,9 +0,0 @@
-package settings;
-
-public class SettingsParseException extends Exception{
-	private static final long serialVersionUID = 7793612894551543259L;
-
-	public SettingsParseException(Settings s, String message) {
-		super(s.getClass().getSimpleName() + "::" + message);
-	}
-}
