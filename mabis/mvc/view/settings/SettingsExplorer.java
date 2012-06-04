91677170704c28cfdfe0535f6e06418e74c80c5e
kornicameister <kornicameister@gmail.com>
2012-06-04 02:17:49 +0200

    starting to develop settings exploreR

---
+++ mabis/mvc/view/settings/SettingsExplorer.java
@@ -0,0 +1,17 @@
+package mvc.view.settings;
+
+public class SettingsExplorer {
+
+	public SettingsExplorer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
